b508c1031429366eaf7f05c4f4eaf8d7f4e3c9af
sukso96100 <sukso96100@gmail.com>
2018-08-25 00:23:41 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,86 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import MonacoEditor from "react-monaco-editor";
+import Peer from "peerjs";
+
+const editorStyle = {
+  textAlign: "left"
+};
+
+const idDisplayStyle = {
+  padding: "16px"
+};
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.peer = undefined;
+    this.connection = undefined;
+    this.editor = undefined;
+    this.state = {
+      peerId: ""
+    };
+  }
+
+  componentDidMount() {
+    this.peer = new Peer();
+    console.log(this.peer);
+    this.peer.on("open", id => {
+      this.setState({
+        peerId: id
+      });
+      console.log("ID: " + id);
+    });
+  }
+  
+  onReceiveChanges(data){
+    console.log(data);
+  }
+
+  connect(){
+    console.log(this.refs.idinput.value);
+    this.connection = this.peer.connect(this.refs.idinput.value);
+    this.connection.on('open', ()=>{
+      this.connection.send(`hello! from ${this.state.peerId}`);
+    });
+    this.connection.on('connect', (conn)=>{
+      conn.on('data', this.onReceiveChanges);
+    })
+  }
+
+  editorDidMount(editor, monaco) {
+    // editor
+    // this.editor = editor;
+    // this.mMonaco = monaco;
+  }
+
+  onChange(newValue, event) {
+    console.log("onChange", newValue, event);
+  }
+  render() {
+    return (
+      <div className="App" style={{ textAlign: "start" }}>
+        <div style={idDisplayStyle}>
+          <p>Connection ID: {this.state.peerId}</p>
+          <input type='text' placeholder='id of room to connect' ref='idinput'></input>
+          <button onClick={this.connect.bind(this)}>Connect</button>
+        </div>
+        <MonacoEditor
+          style={editorStyle}
+          width="800"
+          height="600"
+          language="javascript"
+          theme="vs-dark"
+          // value={code}
+          // options={options}
+          onChange={this.onChange}
+          editorDidMount={this.editorDidMount}
+          ref="monaco"
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
